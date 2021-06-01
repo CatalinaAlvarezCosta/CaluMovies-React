c7e7efa6ca8d7cc61499b8aecfb92f9d535e07e8
Catalina Alvarez Costa <cata.ac2001@gmail.com>
2021-06-01 17:01:39 -0300

    cambio del diseño

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -5,17 +5,17 @@ import {Navbar ,Nav, FormControl,Form,Button} from 'react-bootstrap'
 function NavBar (){
 return (
     <Navbar expand="lg" className="navbar-cont" >
-    <Navbar.Brand href="#home" className="navbar-logo"  >Calu Cakes</Navbar.Brand>
+    <Navbar.Brand href="#home" className="navbar-logo"  >Calu Movies</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#tortas">Tortas</Nav.Link>
+        <Nav.Link href="#tortas">Peliculas</Nav.Link>
         <Nav.Link href="#nosotras">Nosotras</Nav.Link>
         <Nav.Link href="#contacto">Contacto</Nav.Link>
       </Nav>
       <Form inline>
-        <FormControl type="text" placeholder="Busca tu torta" className="mr-sm-2" />
+        <FormControl type="text" placeholder="Busca tu pelicula favorita" className="mr-sm-2" />
         <Button variant="outline-light">Buscar</Button>
       </Form>
     </Navbar.Collapse>
