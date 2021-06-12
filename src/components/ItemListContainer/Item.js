450301870c7ad0855c63d6e684ee2731d4e32266
Catalina Alvarez Costa <cata.ac2001@gmail.com>
2021-06-12 19:17:01 -0300

    Cambio Items

--- src/components/ItemListContainer/Item.js
+++ src/components/ItemListContainer/Item.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import './Item.css';
+import ItemCount from '../ItemCount'
+const data= [
+  {titulo:'Mujer Bonita',
+  subtitulo: 'Un elegante empresario,Edward Lewis,hace el esmerado intento de adaptar a una mujer llamada Vivían Ward a las reglas de una sofisticada empresa de negocios.',
+  stock: 4,
+  src: "Fotos/mujerbonita.jpg",
+  precio:1000},
+{
+  titulo:'Notting Hill',
+  subtitulo: 'Anna Scott, estrella de cine, y William Thacker, un biblotecario, se conocen en un lugar llamado Notting Hill. Nace un inusitado romance, obstaculizado la prensa y sus propias diferencias.',
+  stock: 8,
+  src: "Fotos/nottinghill.jpg",
+  precio:1200
+},
+{
+  titulo:'Realmente amor',
+  subtitulo: 'Las vidas de varias parejas se entrecruzan en Londres, poco antes de la Navidad.Esta encantadora comedia romántica cuenta diez historias de amor entrelazadas.',
+  stock: 9,
+  src: "Fotos/loveactually.jpg",
+  precio:870
+}]
+function Items (){
 
-function Items (props){
-
-    const {children,titulo,subtitulo,src,precio} = props
-    return    <Card ><Card.Img variant="top" src={src} style={{ height: '20rem' }} className="cardEstilo"/>
+    return    <Card >{data.map((producto,i) => 
+    <div key={i} >  <Card.Img variant="top" src={producto.src} style={{ height: '20rem' }} className="cardEstilo"/>
     <Card.Body>
-      <Card.Title className="tituloCard">{titulo}</Card.Title>
+      <Card.Title className="tituloCard">{producto.titulo}</Card.Title>
       <Card.Text className="precio">
-        <h5>${precio}</h5>
+        <h5>${producto.precio}</h5>
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-     {children}
-    </Card.Footer></Card> 
+    <ItemCount min={1} max={producto.stock}/>
+    </Card.Footer> </div>)}
+     </Card> 
 }
 
 export default Items
--- src/components/ItemListContainer/ItemList.js
+++ src/components/ItemListContainer/ItemList.js
@@ -1,41 +1,10 @@
 import React from 'react'
 import Items from './Item'
-import ItemCount from '../ItemCount'
 
-const dataCardUno = {
-    titulo:'Mujer Bonita',
-    subtitulo: 'Un elegante empresario,Edward Lewis,hace el esmerado intento de adaptar a una mujer llamada Vivían Ward a las reglas de una sofisticada empresa de negocios.',
-    stock: 4,
-    src: "Fotos/mujerbonita.jpg",
-    precio:1000
-}
-const dataCardDos = {
-    titulo:'Notting Hill',
-    subtitulo: 'Anna Scott, estrella de cine, y William Thacker, un biblotecario, se conocen en un lugar llamado Notting Hill. Nace un inusitado romance, obstaculizado la prensa y sus propias diferencias.',
-    stock: 8,
-    src: "Fotos/nottinghill.jpg",
-    precio:1200
-}
-const dataCardTres = {
-    titulo:'Realmente amor',
-    subtitulo: 'Las vidas de varias parejas se entrecruzan en Londres, poco antes de la Navidad.Esta encantadora comedia romántica cuenta diez historias de amor entrelazadas.',
-    stock: 9,
-    src: "Fotos/loveactually.jpg",
-    precio:870
-}
 
 function ItemsList (){
     return <div>
-   
-        <Items {...dataCardUno}>
-        <ItemCount min={1} max={dataCardUno.stock}/>
-        </Items>
-        <Items {...dataCardDos}>
-        <ItemCount min={1} max={dataCardDos.stock} />
-        </Items>
-        <Items {...dataCardTres}>
-        <ItemCount min={1} max={dataCardTres.stock} />
-        </Items>
+        <Items/>
    </div>
 }
 export default ItemsList;
