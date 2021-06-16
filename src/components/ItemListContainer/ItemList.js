48763142952c84397db3cf38ff50c99b826bfe4b
Catalina Alvarez Costa <cata.ac2001@gmail.com>
2021-06-15 23:29:28 -0300

    Update ItemListContainer

--- src/components/ItemListContainer/ItemList.js
+++ src/components/ItemListContainer/ItemList.js
@@ -1,39 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {Items} from './Item'
 
 
-export const ItemsList = () =>{
-    const [productos, setProducto] = useState([]);
-    const promiseProductos= () =>{
-        return new Promise ((resolve,reject)=>{
-            setTimeout(() => resolve(
-                    [
-                        {titulo:'Mujer Bonita',
-                        subtitulo: 'Un elegante empresario,Edward Lewis,hace el esmerado intento de adaptar a una mujer llamada Vivían Ward a las reglas de una sofisticada empresa de negocios.',
-                        stock: 4,
-                        src: "Fotos/mujerbonita.jpg",
-                        precio:1000},
-                      {
-                        titulo:'Notting Hill',
-                        subtitulo: 'Anna Scott, estrella de cine, y William Thacker, un biblotecario, se conocen en un lugar llamado Notting Hill. Nace un inusitado romance, obstaculizado la prensa y sus propias diferencias.',
-                        stock: 8,
-                        src: "Fotos/nottinghill.jpg",
-                        precio:1200
-                      },
-                      {
-                        titulo:'Realmente amor',
-                        subtitulo: 'Las vidas de varias parejas se entrecruzan en Londres, poco antes de la Navidad.Esta encantadora comedia romántica cuenta diez historias de amor entrelazadas.',
-                        stock: 9,
-                        src: "Fotos/loveactually.jpg",
-                        precio:870
-                      }]),
-                2000
-            ); }); };
-        useEffect(() =>{
-            promiseProductos().then((productos) => {setProducto(productos) });
-        },[]); 
+export const ItemsList = ({producto,i}) =>{
+    
     return <div>
-        {productos.map((producto,i) => 
-    <Items key={i} producto={producto}/> )}
+    <Items key={i} producto={producto}/> 
    </div>
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React ,{ useEffect, useState } from 'react'
 import {ItemsList} from './ItemList'
 import CardColumns from 'react-bootstrap/CardColumns'
+import {Items} from './Item'
+
 
 function ItemListContainer () {
-    return <div> <CardColumns> 
-        <ItemsList/>
+    const [productos, setProducto] = useState([]);
+    const promiseProductos= () =>{
+        return new Promise ((resolve,reject)=>{
+            setTimeout(() => resolve(
+                    [
+                        {titulo:'Mujer Bonita',
+                        subtitulo: 'Un elegante empresario,Edward Lewis,hace el esmerado intento de adaptar a una mujer llamada Vivían Ward a las reglas de una sofisticada empresa de negocios.',
+                        stock: 4,
+                        src: "Fotos/mujerbonita.jpg",
+                        precio:1000},
+                      {
+                        titulo:'Notting Hill',
+                        subtitulo: 'Anna Scott, estrella de cine, y William Thacker, un biblotecario, se conocen en un lugar llamado Notting Hill. Nace un inusitado romance, obstaculizado la prensa y sus propias diferencias.',
+                        stock: 8,
+                        src: "Fotos/nottinghill.jpg",
+                        precio:1200
+                      },
+                      {
+                        titulo:'Realmente amor',
+                        subtitulo: 'Las vidas de varias parejas se entrecruzan en Londres, poco antes de la Navidad.Esta encantadora comedia romántica cuenta diez historias de amor entrelazadas.',
+                        stock: 9,
+                        src: "Fotos/loveactually.jpg",
+                        precio:870
+                      }]),
+                2000
+            ); }); };
+        useEffect(() =>{
+            promiseProductos().then((productos) => {setProducto(productos) });
+        },[]); 
+    return <div> <CardColumns>  {productos.map((producto,i) => 
+        <ItemsList key={i} producto={producto}/>)}
         </CardColumns></div>
 }
 export default ItemListContainer;
