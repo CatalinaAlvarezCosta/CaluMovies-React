60272c2a552a423ac1ac503488e063b01dd859ef
Catalina Alvarez Costa <cata.ac2001@gmail.com>
2021-06-21 21:17:28 -0300

    Filter Problem

--- src/components/ItemListContainer/ItemList.js
+++ src/components/ItemListContainer/ItemList.js
@@ -1,27 +1,14 @@
-import React ,{ useEffect, useState }  from 'react'
+import React ,{ useState}  from 'react'
 import {Items} from './Item' 
 import Button from 'bootstrap'
-export const ItemsList = ({producto}) =>{
-    const [productos, setProducto] = useState([]);
-    const [filter,setFilter] = useState ('')
-    
 
-    const promiseProductos= () =>{
-        return new Promise ((resolve,reject)=>{
-            setTimeout(() => resolve(
-                    producto),
-                2000
-            ); }); };
-      const filterProductos = () => {
-            return filter === '' ? producto.filter ( productos => productos.type === filter) : productos
-            }
-        useEffect(() =>{
-            promiseProductos().then((productos) => {setProducto(productos) });
-        },[]); 
-        
+export const ItemsList = ({producto}) =>{
+    const [filter,setFilter] = useState ('')
+    const filterProductos = data => {
+        return filter === '' ? data.filter ( producto => producto.type === filter) : data
+        };
     return <section className="itemList"> 
-    <Button onClick = {() => setFilter ('Romanticas')}>  </Button>
-     {
-        filterProductos(productos).map((producto,i) => <Items producto={producto} key={i}/>)}
+     <Button onClick = {() => setFilter ('Romanticas')}> </Button>
+     {filterProductos(producto).map((producto,i) => <Items producto={producto} key={i}/>)}
    </section>
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,36 +1,52 @@
-import React from 'react'
+import React ,{ useEffect, useState } from 'react'
 import {ItemsList} from './ItemList'
 import CardColumns from 'react-bootstrap/CardColumns'
 
+
 function ItemListContainer () {
-    const data = [
-                        {titulo:'Mujer Bonita',
-                        subtitulo: 'Un elegante empresario,Edward Lewis,hace el esmerado intento de adaptar a una mujer llamada Vivían Ward a las reglas de una sofisticada empresa de negocios.',
-                        stock: 4,
-                        src: "Fotos/mujerbonita.jpg",
-                        precio:1000,
-                        type:'Romantica',
-                        id:1},
-                      {
-                        titulo:'Notting Hill',
-                        subtitulo: 'Anna Scott, estrella de cine, y William Thacker, un biblotecario, se conocen en un lugar llamado Notting Hill. Nace un inusitado romance, obstaculizado la prensa y sus propias diferencias.',
-                        stock: 8,
-                        src: "Fotos/nottinghill.jpg",
-                        precio:1200,
-                        type:'Romantica',
-                        id:2
-                      },
-                      {
-                        titulo:'Realmente amor',
-                        subtitulo: 'Las vidas de varias parejas se entrecruzan en Londres, poco antes de la Navidad.Esta encantadora comedia romántica cuenta diez historias de amor entrelazadas.',
-                        stock: 9,
-                        src: "Fotos/loveactually.jpg",
-                        precio:870,
-                        type:'Comedia',
-                        id:3
-                      }]
+  const [productos, setProducto] = useState([]);
+ 
+
+
+
+  const promiseProductos= () =>{
+      return new Promise ((resolve,reject)=>{
+          setTimeout(() => resolve(
+            [
+              {titulo:'Mujer Bonita',
+              subtitulo: 'Un elegante empresario,Edward Lewis,hace el esmerado intento de adaptar a una mujer llamada Vivían Ward a las reglas de una sofisticada empresa de negocios.',
+              stock: 4,
+              src: "Fotos/mujerbonita.jpg",
+              precio:1000,
+              type:'Romantica',
+              id:1},
+            {
+              titulo:'Notting Hill',
+              subtitulo: 'Anna Scott, estrella de cine, y William Thacker, un biblotecario, se conocen en un lugar llamado Notting Hill. Nace un inusitado romance, obstaculizado la prensa y sus propias diferencias.',
+              stock: 8,
+              src: "Fotos/nottinghill.jpg",
+              precio:1200,
+              type:'Romantica',
+              id:2
+            },
+            {
+              titulo:'Realmente amor',
+              subtitulo: 'Las vidas de varias parejas se entrecruzan en Londres, poco antes de la Navidad.Esta encantadora comedia romántica cuenta diez historias de amor entrelazadas.',
+              stock: 9,
+              src: "Fotos/loveactually.jpg",
+              precio:870,
+              type:'Comedia',
+              id:3
+            }]),
+              2000
+          ); }); };
+      useEffect(() =>{
+          promiseProductos().then((productos) => {setProducto(productos) });
+      },[]); 
+      
+    
     return <div> <CardColumns> 
-      <ItemsList producto={data}/>
+           <ItemsList producto={productos}/> 
         </CardColumns></div>
 }
 export default ItemListContainer;
